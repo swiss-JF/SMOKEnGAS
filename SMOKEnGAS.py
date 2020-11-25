a4515fbb86a26dd8a69cf746fcaa300fb9a7a2b1
Alexander Pervakov <frost.nzcr4@jagmort.com>
2020-11-25 16:33:28 +0300

    Add main wrapper
    Use `main()` so import of your package as third-party library doesn't hurt anyone. See https://stackoverflow.com/questions/419163/what-does-if-name-main-do

--- SMOKEnGAS.py
+++ SMOKEnGAS.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python3
 import csv
+import logging
 import time
 from datetime import datetime
-from pms5003 import PMS5003, ReadTimeoutError
-import logging
+
 import gas
-
-logging.basicConfig(
-    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+from pms5003 import PMS5003, ReadTimeoutError
 
 
-pms5003 = PMS5003()
-time.sleep(1.0)
+def main():
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
 
-try:
-    while True:
-        try:
-            with open('pms5003.csv', 'a', newline='') as csvfile:
-                writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                readings = pms5003.read()  # PMS5003Data instance
-                dt = datetime.now()
-				readings1 = pms5003.read()
-				readings2 = gas.read_all() #gas data instance
-				writer.writerow([dt.isoformat()] + list(readings1.data[:-2] + [readings2.oxidising, readings2.reducing, readings2.nh3, readings2.adc])
-        except ReadTimeoutError:
-            pms5003 = PMS5003()
-except KeyboardInterrupt:
-    pass
+
+    pms5003 = PMS5003()
+    time.sleep(1.0)
+
+    try:
+        while True:
+            try:
+                with open('pms5003.csv', 'a', newline='') as csvfile:
+                    writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    readings = pms5003.read()  # PMS5003Data instance
+                    dt = datetime.now()
+                    readings1 = pms5003.read()
+                    readings2 = gas.read_all() #gas data instance
+                    writer.writerow([dt.isoformat()] + list(readings1.data[:-2] + [readings2.oxidising, readings2.reducing, readings2.nh3, readings2.adc])
+            except ReadTimeoutError:
+                pms5003 = PMS5003()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
